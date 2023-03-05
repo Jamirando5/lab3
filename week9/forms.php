f34925ae5f0ebd2f84875902348a104b2232a2a9
Jamirando5 <119831201+Jamirando5@users.noreply.github.com>
2023-03-05 20:42:03 +0800

    Update forms.php

--- week9/forms.php
+++ week9/forms.php
@@ -39,36 +39,36 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
   }
   
-  if (empty($_POST["email"])) {
+  if (empty($_POST["Email"])) {
     $emailErr = "Email is required";
   } else {
-    $email = test_input($_POST["email"]);
+    $email = test_input($_POST["Email"]);
     // check if e-mail address is well-formed
     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
       $emailErr = "Invalid email format";
     }
   }
     
-  if (empty($_POST["website"])) {
+  if (empty($_POST["Website"])) {
     $website = "";
   } else {
-    $website = test_input($_POST["website"]);
+    $website = test_input($_POST["Website"]);
     // check if URL address syntax is valid (this regular expression also allows dashes in the URL)
     if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$website)) {
       $websiteErr = "Invalid URL";
     }
   }
 
-  if (empty($_POST["comment"])) {
+  if (empty($_POST["Comment"])) {
     $comment = "";
   } else {
-    $comment = test_input($_POST["comment"]);
+    $comment = test_input($_POST["Comment"]);
   }
 
-  if (empty($_POST["gender"])) {
+  if (empty($_POST["Gender"])) {
     $genderErr = "Gender is required";
   } else {
-    $gender = test_input($_POST["gender"]);
+    $gender = test_input($_POST["Gender"]);
   }
 }
 
