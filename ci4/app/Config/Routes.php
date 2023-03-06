ca4c356eb6005e516259aa5f40929518ad8dcd4e
Jamirando <jamirando@apc.edu.ph>
2023-03-06 21:46:39 +0800

    update

--- ci4/app/Config/Routes.php
+++ ci4/app/Config/Routes.php
@@ -32,7 +32,7 @@ $routes->set404Override();
 $routes->get('/', 'Home::index');
 
 use App\Controllers\Guest;
-use App\Controller\News;
+use App\Controllers\News;
 use App\Controllers\Pages;
 
 $routes->match(['get', 'post'], 'guest/create', [Guest::class, 'create']);
@@ -40,6 +40,8 @@ $routes->get('guest', [Guest::class, 'index']);
 
 $routes->match(['get', 'post'], 'news/create', [News::class, 'create']);
 $routes->get('news/(:segment)', [News::class, 'view']);
+$routes->get('news',[News::class, 'index']);
+
 $routes->get('pages', [Pages::class, 'index']);
 $routes->get('pages', [Pages::class, 'index']);
 $routes->get('(:segment)', [Pages::class, 'view']);
--- ci4/app/Controllers/News.php
+++ ci4/app/Controllers/News.php
@@ -3,11 +3,40 @@
 namespace App\Controllers;
 
 use App\Models\NewsModel;
+use CodeIgniter\Exceptions\PageNotFoundException;
 
 class News extends BaseController
 {
-    // ...
+    public function index()
+    {
+        $model = model(NewsModel::class);
 
+        $data = [
+            'news'  => $model->getNews(),
+            'title' => 'News archive',
+        ];
+
+        return view('templates/header', $data)
+            . view('news/index')
+            . view('templates/footer');
+    }
+
+    public function view($slug = null)
+    {
+        $model = model(NewsModel::class);
+
+        $data['news'] = $model->getNews($slug);
+
+        if (empty($data['news'])) {
+            throw new PageNotFoundException('Cannot find the news item: ' . $slug);
+        }
+
+        $data['title'] = $data['news']['title'];
+
+        return view('templates/header', $data)
+            . view('news/view')
+            . view('templates/footer');
+    }
     public function create()
     {
         helper('form');
@@ -46,3 +75,4 @@ class News extends BaseController
             . view('templates/footer');
     }
 }
+?>
