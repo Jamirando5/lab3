22dbbde92fb73f88d31fddac32498ff2283fa608
Jamirando <jamirando@apc.edu.ph>
2023-03-06 20:35:57 +0800

    update

--- ci4/app/Views/guest/create.php
+++ ci4/app/Views/guest/create.php
@@ -3,27 +3,27 @@
 <?= session()->getFlashdata('error') ?>
 <?= validation_list_errors() ?>
 
-<form action="/news/create" method="post">
+<form action="/guest/create" method="post">
     <?= csrf_field() ?>
 
     <label  for="Name">Name</label>
-    <input type="input" name="Name" value="<?= set_value('name') ?>">
+    <input type="input" name="Name" value="<?= set_value('Name') ?>">
     <br>
 
     <label  for="Email">Email</label>
-    <input type="input" name="Email" value="<?= set_value('email') ?>">
+    <input type="input" name="Email" value="<?= set_value('Email') ?>">
     <br>
 
     <label  for="Website">Website</label>
-    <input type="input" name="Website" value="<?= set_value('website') ?>">
+    <input type="input" name="Website" value="<?= set_value('Website') ?>">
     <br>
 
     <label  for="Comment">Comment</label>
-    <textarea name="Comment" cols="45" rows="4"><?= set_value('comment') ?></textarea>
+    <textarea name="Comment" cols="45" rows="4"><?= set_value('Comment') ?></textarea>
     <br>
 
     <label  for="Gender">Gender</label>
-    <input type="input" name="Gender" value="<?= set_value('gender') ?>">
+    <input type="input" name="Gender" value="<?= set_value('Gender') ?>">
     <br>
 
     <input type="submit" name="submit" value="Create guest entry">
