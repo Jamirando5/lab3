f929d96be78e328bc49741564529e68bbdb61d38
Jamirando <jamirando@apc.edu.ph>
2023-03-06 09:48:53 +0800

    ci4 update

---
+++ ci4/app/Views/pages/home.php
@@ -0,0 +1,89 @@
+
+<body>
+    <div id ="home"  class="hero">
+        <nav>
+            <h2 class="logo">Stric<span>To</span></h2>
+            <ul>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#skills">Skills</a></li>
+                <li><a href="#socials">Contact me</a></li>
+                <li><a href="http://localhost/lab3/ci4/public/time">Time</a></li>
+                <li><a href="http://localhost/lab3/ci4/public/hobbies">Hobbies</a></li>
+            </ul>
+            <a href="forms.php" class="btn">PHP FORMS</a>
+        </nav>
+
+        <div class="content">
+            <h4>Hello, my name is</h4>
+            <h1>Jonlord <span>Mirando</span></h1>
+            <h3>I'm a Computer Science student</h3>
+        </div>
+    </div>
+    <section id="about" class="about">
+        <div class="main">
+            <img src="images/Portrait.jpg">
+            <div class="about-text">
+                <h2>Who am <span>I?</span></h2>
+                <h5>Computer Science student</h5>
+                <p>I'm Jonlord Mirando, 2nd year Computer Science student
+                    from Asia Pacific College. I'm born and raised from the
+                    ciy of Makati. The last school that I attended was Polytechnic
+                    University of the Philippines. I also have a slight Engineering
+                    background since my former course was Electrical Engineering before
+                    transferring to Asia Pacific College as a Computer Science student.
+                    My goal is that 1 day I would become successful and well known
+                    in the IT and business industry. As Steve Jobs said, We're here 
+                    to put a dent in the universe. Otherwise why else even be here?
+                </p>
+            </div>
+        </div>
+    </section>
+    <div id = "skills" class="skills">
+        <div class="title">
+            <h2>Skills</h2>
+        </div>
+        <div class="box">
+            <div class="card">
+                <i class="fa-brands fa-windows"></i>
+                <h5>Personal Computer Assembly</h5>
+                <div class="pra">
+                <p>I have decent knowledge in personal computer assembly</p>
+                 <p style ="text-align: center;"></p>
+                </div>
+            </div>
+            <div>
+                <div class="card">
+                    <i class="fa-solid fa-code"></i>
+                    <h5>Programming</h5>
+                    <div class="pra">
+                    <p>I have decent knowledge in Programming languages like Java, Python, and HTML</p>
+                     <p style ="text-align: center;"></p>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div class="card">
+                    <i class="fa-solid fa-computer"></i>
+                    <h5>Web Design</h5>
+                    <div class="pra">
+                    <p>I have decent knowledge in Web Design</p>
+                     <p style ="text-align: center;"></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <footer>
+        <p>Socials</p>
+        <p>For more information about myself, click the links below
+            and follow me:
+        </p>
+        <div id = "socials" class ="Social">
+            <a href="https://www.facebook.com/Jlorddd/"><i class="fa-brands fa-square-facebook"></i></a>
+            <a href="https://www.instagram.com/_chinkymoltisanti/"><i class="fa-brands fa-square-instagram"></i></a>
+            <a href="https://twitter.com/TheLifeofJel"><i class="fa-brands fa-square-twitter"></i></a>
+        </div>
+    </footer>    
+<body>
+</html>
